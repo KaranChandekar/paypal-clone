8081200c90de88f9c6d1acb5876688c1fb680e85
Karan <karanchandekar431@gmail.com>
2022-10-01 21:24:42 +0530

    made login page work with javascript

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Pages
+document.querySelectorAll(".logo").forEach((logo) => {
+  logo.addEventListener("click", () => {
+    document.querySelector(".front-page").style.display = "block";
+    document.querySelector(".login-page").style.display = "none";
+    document.querySelector(".signup-page").style.display = "none";
+  });
+});
+
+document.querySelectorAll(".login").forEach((loginBtn) => {
+  loginBtn.addEventListener("click", () => {
+    document.querySelector(".front-page").style.display = "none";
+    document.querySelector(".login-page").style.display = "block";
+    document.querySelector(".signup-page").style.display = "none";
+  });
+});
+// End of Pages
+
 // Navigation
 const dropdownItems = document.querySelectorAll(".dropdown-hover");
 
