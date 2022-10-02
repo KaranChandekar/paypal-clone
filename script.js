eabcdb3f5502effb8b3abd3b7150ca14f21acf08
Karan <karanchandekar431@gmail.com>
2022-10-02 12:02:02 +0530

    made sign up page work using javascript

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ document.querySelectorAll(".login").forEach((loginBtn) => {
     document.querySelector(".signup-page").style.display = "none";
   });
 });
+
+document.querySelectorAll(".signup").forEach((signupBtn) => {
+  signupBtn.addEventListener("click", () => {
+    document.querySelector(".front-page").style.display = "none";
+    document.querySelector(".login-page").style.display = "none";
+    document.querySelector(".signup-page").style.display = "flex";
+  });
+});
 // End of Pages
 
 // Navigation
